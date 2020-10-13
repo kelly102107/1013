6bea9eede12cf521339ece6a861e6dd231b36c8a
kelly102107 <71689279+kelly102107@users.noreply.github.com>
2020-10-13 14:35:34 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,48 @@
+input.onGesture(Gesture.LogoUp, function () {
+    basic.showString("1")
+    basic.pause(100)
+    basic.clearScreen()
+})
+input.onGesture(Gesture.TiltLeft, function () {
+    basic.showString("5")
+    basic.pause(100)
+    basic.clearScreen()
+})
+input.onGesture(Gesture.ThreeG, function () {
+    basic.showString("8")
+    basic.pause(100)
+    basic.clearScreen()
+})
+input.onGesture(Gesture.ScreenDown, function () {
+    basic.showString("4")
+    basic.pause(100)
+    basic.clearScreen()
+})
+input.onGesture(Gesture.FreeFall, function () {
+    basic.showString("7")
+    basic.pause(100)
+    basic.clearScreen()
+})
+input.onGesture(Gesture.Shake, function () {
+    basic.showString("0")
+    basic.pause(100)
+    basic.clearScreen()
+})
+input.onGesture(Gesture.ScreenUp, function () {
+    basic.showString("3")
+    basic.pause(100)
+    basic.clearScreen()
+})
+input.onGesture(Gesture.LogoDown, function () {
+    basic.showString("2")
+    basic.pause(100)
+    basic.clearScreen()
+})
+input.onGesture(Gesture.TiltRight, function () {
+    basic.showString("6")
+    basic.pause(100)
+    basic.clearScreen()
+})
+basic.forever(function () {
+	
+})
